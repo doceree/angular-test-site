e93a321ff01ce9e89679b00adb4331e4a4756881
amrit991 <amrit.raj@doceree.com>
2020-04-24 15:02:29 +0530

    new look

---
+++ src/app/app.component.ts
@@ -0,0 +1,42 @@
+import { Component } from '@angular/core';
+import { UserService } from "./core/services/user.service";
+import { Router, Event, NavigationStart, NavigationEnd, NavigationError } from '@angular/router';
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Well-being Today';
+  showCarousel: boolean = false;
+  public slides = [
+    { src: "https://source.unsplash.com/collection/9990991"},
+    { src: "https://source.unsplash.com/collection/9990991"},
+    { src: "https://source.unsplash.com/collection/9990991"},
+    { src: "https://source.unsplash.com/collection/9990991"}
+  ]
+  constructor(private userService: UserService, private router: Router) {
+    this.router.events.subscribe((event: Event) => {
+      if (event instanceof NavigationStart) {
+          // Show loading indicator
+          console.log('nav start',event);
+      }
+
+      if (event instanceof NavigationEnd) {
+          // Hide loading indicator
+          console.log('nav end' ,event);
+          if( event.urlAfterRedirects == '/dashboard') {
+            this.showCarousel = true;
+          } else {
+            this.showCarousel = false;
+          }
+      }
+
+      if (event instanceof NavigationError) {
+          // Hide loading indicator
+          // Present error to user
+          console.log(event.error);
+      }
+  });
+   }
+}
